84bd3071efcd749611897e259c54070ac447e5f4
Riska <riska607@gmail.com>
2026-09-07 23:42:39 +0700

    Tambah halaman login

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <div class="container">
+        <h1>Login</h1>
+
+        <form>
+            <input type="text" placeholder="Username">
+            <br><br>
+
+            <input type="password" placeholder="Password">
+            <br><br>
+
+            <button type="submit">Login</button>
+        </form>
+    </div>
+
+</body>
+</html>
